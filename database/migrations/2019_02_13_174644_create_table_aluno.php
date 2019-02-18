0cdb0094e23918f3beb5b44f7d234dde79d04d00
jackoliveiramanaus <40442021+jackoliveiramanaus@users.noreply.github.com>
2019-02-18 11:36:35 -0300

    Create 2019_02_13_174644_create_table_aluno.php

---
+++ database/migrations/2019_02_13_174644_create_table_aluno.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableAluno extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aluno', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('data_nascimento');
+            $table->string('logradouro');
+            $table->string('numero');
+            $table->string('bairro');
+            $table->string('cidade');
+            $table->string('estado');
+            $table->string('cep');
+            $table->integer('curso_id')->unsigned();
+            $table->foreign('curso_id')->references('id')->on('curso'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aluno');
+    }
+}
