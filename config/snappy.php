790988afae1b6aeb8088e121795383a2e14b6f94
jackoliveiramanaus <40442021+jackoliveiramanaus@users.noreply.github.com>
2019-02-18 11:35:54 -0300

    Create snappy.php

---
+++ config/snappy.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+
+
+    'pdf' => array(
+        'enabled' => true,
+        'binary'  => 'C:\laragon\www\flex\deppdf\wkhtmltopdf',
+        'timeout' => false,
+        'options' => array(),
+        'env'     => array(),
+    ),
+    'image' => array(
+        'enabled' => true,
+        'binary'  => 'C:\laragon\www\flex\deppdfwkhtmltoimage',
+        'timeout' => false,
+        'options' => array(),
+        'env'     => array(),
+    ),
+
+
+);
