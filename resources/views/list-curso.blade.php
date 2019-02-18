32bcdbc3202f426414c033be907880724aee57e7
jackoliveiramanaus <40442021+jackoliveiramanaus@users.noreply.github.com>
2019-02-18 11:44:15 -0300

    Create list-curso.blade.php

---
+++ resources/views/list-curso.blade.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Flex Jack</title>
+        
+        <!-- Favicon -->
+        <link href="{{URL::asset('img/favicon.ico')}}" rel="shortcut icon">
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"> 
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="{{URL::asset('css/style.css')}}" rel="stylesheet" type="text/css" /> 
+        <link href="{{URL::asset('css/lightbox.css')}}" rel="stylesheet" type="text/css" /> 
+
+        <!-- JavaScript -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="{{URL::asset('js/ajax.js')}}"></script>
+        <script src="{{URL::asset('js/lightbox.js')}}"></script>
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse navbar-fixed-top">       
+            <div class="navbar-header">
+                <a class="navbar-brand" href="{{url('/')}}" 
+                   title="Página Inicial" style="margin-top: -3px">
+                    <img src="{{URL::asset('img/logo.png')}}"></a>
+                <button type="button" class="navbar-toggle" 
+                        data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>               
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav" id="link-white">
+                    <li>
+                        <a href="http://localhost:8000/" style="text-decoration: none">
+                            <span class="glyphicon glyphicon-home"></span> 
+                            <span id="underline">Painel</span>
+                        </a>
+                    </li>
+                   
+                    
+                
+            </div>       
+        </nav> 
+        @if (session('message'))
+        <div class="alert alert-success alert-dismissible">
+            <a href="#" class="close" 
+               data-dismiss="alert"
+               aria-label="close">&times;</a>
+            {{ session('message') }}
+        </div>
+        @endif
+        <div id="line-one">   
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12" id="center"> 
+                        <h1><b>Cusos</b></h1>
+                        <br>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <ol class="breadcrumb">
+                            <li><a href="http://localhost:8000/">Home</a></li>                  
+                            <li class="active">Curso</li>
+                        </ol>
+                        <br>
+                        <a href="{{route('curso.create')}}" 
+                           class="btn btn-default btn-sm pull-right">
+                            <span class="glyphicon glyphicon-plus"></span> Cadastrar</a>
+                       
+                        
+                    </div>           
+                </div>
+                <div class="row">
+                    <div class="col-md-12">   
+                        <br />
+                        <h4 id="center"><b>CURSOS CADASTRADOS ({{$total}})</b></h4>
+                        <br>
+                        <div class="table-responsive">
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th id="center">Código</th>
+                                        <th>Nome</th>
+                                        <th id="center">Professor</th>                 
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($curso as $curso)
+                                    <tr>
+                                        <td id="center">{{$curso->id}}</td>
+                                        <td title="Nome">{{$curso->nome}}</td>
+                                        <td title="Professor" id="center">{{$curso->professor_id}}</td>
+                                        <td id="center">
+                                            </td>
+                                        <td id="center">
+                                            <a href="{{route('curso.edit', $curso->id)}}" 
+                                               data-toggle="tooltip" 
+                                               data-placement="top"
+                                               title="Editar"><i class="fa fa-pencil"></i></a>
+                                            &nbsp;<form style="display: inline-block;" method="POST" 
+                                                        action="{{route('curso.destroy', $curso->id)}}"                                                        
+                                                        data-toggle="tooltip" data-placement="top"
+                                                        title="Excluir" 
+                                                        onsubmit="return confirm('Confirma exclusão?')">
+                                                {{method_field('DELETE')}}{{ csrf_field() }}                                                
+                                                <button type="submit" style="background-color: #fff">
+                                                    <a><i class="fa fa-trash-o"></i></a>                                                    
+                                                </button></form></td>               
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <img src="{{URL::asset('img/subir.png')}}" 
+                 id="up" 
+                 style="display: none;" 
+                 alt="Ícone Subir ao Topo" 
+                 title="Subir ao topo?">
+                 <html>
+    <head>
+    
+            </body>
+            </html>
+
