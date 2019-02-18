309d04dbf893bf1b1089e68067e5c9ae112391ba
jackoliveiramanaus <40442021+jackoliveiramanaus@users.noreply.github.com>
2019-02-18 11:43:53 -0300

    Create include-aluno.blade.php

---
+++ resources/views/include-aluno.blade.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Flex Jack</title>
+
+        <!-- Favicon -->
+        <link href="{{URL::asset('img/favicon.ico')}}" rel="shortcut icon">
+
+        <!-- Fonts -->        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"> 
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="{{URL::asset('css/style.css')}}" rel="stylesheet" type="text/css" />       
+
+        <!-- JavaScript -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="{{URL::asset('js/ajax.js')}}"></script>        
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse navbar-fixed-top">       
+            <div class="navbar-header">
+                <a class="navbar-brand" href="{{url('/')}}" 
+                   title="Página Inicial" style="margin-top: -3px">
+                    <img src="{{URL::asset('img/logo.png')}}"></a>
+                <button type="button" class="navbar-toggle" 
+                        data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>               
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav" id="link-white">
+                    <li>
+                        <a href="http://localhost:8000/" style="text-decoration: none">
+                            <span class="glyphicon glyphicon-home"></span> 
+                            <span id="underline">Painel</span>
+                        </a>
+                    </li>
+                   
+                    
+            </div>       
+        </nav>       
+        <div id="line-one">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12" id="center">              
+                        <h1><b>Cadastrar Aluno</b></h1>
+                        <br>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <ol class="breadcrumb">
+                            <li><a href="http://localhost:8000/">Home</a></li>                  
+                            <li><a href="{{route('aluno.index')}}">Aluno</a></li><head>
+                            <li><a href="{{route('aluno.index')}}">Cadastrar Aluno</a></li> 
+    <title>ViaCEP Webservice</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <!-- Adicionando JQuery -->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"
+            integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+            crossorigin="anonymous"></script>
+
+    <!-- Adicionando Javascript -->
+    <script type="text/javascript" >
+
+        $(document).ready(function() {
+
+            function limpa_formulário_cep() {
+                // Limpa valores do formulário de cep.
+                $("#rlogradouro").val("");
+                $("#bairro").val("");
+                $("#cidade").val("");
+                $("#estado").val("");
+                
+            }
+            
+            //Quando o campo cep perde o foco.
+            $("#cep").blur(function() {
+
+                //Nova variável "cep" somente com dígitos.
+                var cep = $(this).val().replace(/\D/g, '');
+
+                //Verifica se campo cep possui valor informado.
+                if (cep != "") {
+
+                    //Expressão regular para validar o CEP.
+                    var validacep = /^[0-9]{8}$/;
+
+                    //Valida o formato do CEP.
+                    if(validacep.test(cep)) {
+
+                        //Preenche os campos com "..." enquanto consulta webservice.
+                        $("#logradouro").val("...");
+                        $("#bairro").val("...");
+                        $("#cidade").val("...");
+                        $("#estado").val("...");
+                        
+
+                        //Consulta o webservice viacep.com.br/
+                        $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
+
+                            if (!("erro" in dados)) {
+                                //Atualiza os campos com os valores da consulta.
+                                $("#logradouro").val(dados.logradouro);
+                                $("#bairro").val(dados.bairro);
+                                $("#cidade").val(dados.localidade);
+                                $("#estado").val(dados.uf);
+                                
+                            } //end if.
+                            else {
+                                //CEP pesquisado não foi encontrado.
+                                limpa_formulário_cep();
+                                alert("CEP não encontrado.");
+                            }
+                        });
+                    } //end if.
+                    else {
+                        //cep é inválido.
+                        limpa_formulário_cep();
+                        alert("Formato de CEP inválido.");
+                    }
+                } //end if.
+                else {
+                    //cep sem valor, limpa formulário.
+                    limpa_formulário_cep();
+                }
+            });
+        });
+
+    </script>
+    </head>
+
+    <body>
+    <!-- Inicio do formulario -->
+     
+      <form method="post" 
+      action="{{route('aluno.store')}}"
+      enctype="multipart/form-data">
+      {{ csrf_field() }}
+      <div class="col-md-6">
+        <br> <br/>
+        <label>Nome:
+        <input name="nome" type="text" id="nome" value="" size="50" maxlength="100" /></label><br />
+        <label>Data Nascimento:
+        <input name="data_nascimento" type="text" id="data_nascimento" value="" size="10" maxlength="10" /></label><br />
+        <label>Cep:
+        <input name="cep" type="text" id="cep" value="" size="10" maxlength="9" /></label><br />
+        <label>Logradouro:
+        <input name="logradouro" type="text" id="logradouro" size="50" /></label><br />
+        <label>Numero:
+        <input name="numero" type="text" id="numero" size="10" /></label><br />
+        <label>Bairro:
+        <input name="bairro" type="text" id="bairro" size="40" /></label><br />
+        <label>Cidade:
+        <input name="cidade" type="text" id="cidade" size="40" /></label><br />
+        <label>Estado:
+        <input name="estado" type="text" id="estado" size="30" /></label><br />
+        <label>Curso:
+        <input name="curso_id" type="text" id="curso_id" size="31" /></label><br />
+        
+      </form>
+    
+                        <div class="col-md-12">                   
+                            <button type="reset" class="btn btn-default">
+                                Limpar
+                            </button>
+                            <button type="submit" 
+                                    class="btn btn-warning" id="black">
+                                Cadastrar
+                            </button>
+                        </div>
+                    </form>             
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
